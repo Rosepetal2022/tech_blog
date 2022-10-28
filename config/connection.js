087b587142331c43f6786f34d5993686ab6da7bb
Rachel <marcotter25@yahoo.com>
2022-10-28 09:02:26 -0700

    changed jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,23 +3,14 @@ require('dotenv').config();
 //import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-let sequelize;
-
 //create connection to the database and JAWSDB for Heroku
-if(process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
-} else {
-    sequelize = new Sequelize(
-        process.env.MYSQL_DB, 
-        process.env.MYSQL_USER, 
-        process.env.MYSQL_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql', 
-            port: 3306
-        }
-    );
-}
-
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
+
